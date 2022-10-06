d30b1f9610467bbfef3d89167a0068b9a003f964
Giupeps <dipisagiuseppe92@gmail.com>
2022-10-06 09:53:30 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,18 @@ let i = 0;
 
 function showImage() {
   let getImage = document.querySelector("#imgContainer");
-  getImage.innerHTML = "";
-  getImage.innerHTML += `<img src="https://www.repstatic.it/content/nazionale/img/2022/09/26/213552042-c8a02d29-a0d3-4e23-82d2-0588f22c41d6.jpg">`;
+  //   getImage.innerHTML = "";
+
+  if (getImage.innerHTML != "") {
+    getImage.innerHTML = "";
+  }
+  getImage.innerHTML = `<img src="https://www.repstatic.it/content/nazionale/img/2022/09/26/213552042-c8a02d29-a0d3-4e23-82d2-0588f22c41d6.jpg">`;
 }
 
 function changeColor() {
   mainContainer.style.backgroundColor = colors[i];
   i++;
+  if (i == colors.length) {
+    i = 0;
+  }
 }
